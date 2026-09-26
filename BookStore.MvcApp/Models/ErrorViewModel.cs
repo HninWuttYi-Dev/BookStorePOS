4c9907a560064993fa5afff6916905f0285dae2c
Hnin Wutt Yi <hninwuttyi.mgn@gmail.com>
2026-09-26 14:04:48 +0630

    Delete BookStore.MvcApp directory

--- BookStore.MvcApp/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BookStore.MvcApp.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
